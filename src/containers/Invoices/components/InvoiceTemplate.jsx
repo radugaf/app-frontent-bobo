a23bc467e131552738df193e35114e865ad54653
Airquid <raiobogdanacc@gmail.com>
2021-04-16 02:16:42 +0300

    invoice v2 si tabele

--- src/containers/Invoices/components/InvoiceTemplate.jsx
+++ src/containers/Invoices/components/InvoiceTemplate.jsx
@@ -1,112 +1,263 @@
-import { Fragment, useState } from 'react'
-import { CopyToClipboard } from 'react-copy-to-clipboard'
-import { toast } from 'react-toastify'
-import { Check } from 'react-feather'
+import { Fragment, useState } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { toast } from "react-toastify";
+import { Check } from "react-feather";
 
 import {
-  Card, Col, Button, ButtonToolbar, Table, CardBody,
-} from 'reactstrap';
+  Label,
+  Form,
+  InputGroup,
+  InputGroupAddon,
+  Input,
+  InputGroupText,
+  Row,
+  Card,
+  Col,
+  Button,
+  ButtonToolbar,
+  Table,
+  CardBody,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+} from "reactstrap";
+import { FormGroup } from "@material-ui/core";
 
 const invoiceData = [
-  { title: 'Wordpress Plugin Apollo 200', quantity: 1, cost: 27 },
-  { title: 'Easy Pro Admin Template', quantity: 1, cost: 59 },
-  { title: 'Spirit HTML Template', quantity: 2, cost: 20 },
+  { title: "Wordpress Plugin Apollo 200", quantity: 1, cost: 27 },
+  { title: "Easy Pro Admin Template", quantity: 1, cost: 59 },
+  { title: "Spirit HTML Template", quantity: 2, cost: 20 },
 ];
+
+const invoiceNumber = "#EM000001";
+const invoiceAddress =
+  "Aspirity Creative Web Studio 44 Shirley Ave. West Chicago, IL +8 (224) 243-4543";
 
 const ToastSuccess = () => (
   <Fragment>
-    <div className='toastify-header pb-0'>
-      <div className='title-wrapper'>
-        <h6 className='toast-title'>Copiat!</h6>
+    <div className="toastify-header pb-0">
+      <div className="title-wrapper">
+        <h6 className="toast-title">Copiat!</h6>
       </div>
     </div>
   </Fragment>
-)
-
+);
 
 const InvoiceTemplate = () => {
+  const [copied, setCopied] = useState(false);
 
-  const invoiceNumber='#EM000001';
-  const invoiceAddress='Aspirity Creative Web Studio 44 Shirley Ave. West Chicago, IL +8 (224) 243-4543'
+  const onCopy = () => {
+    setCopied(true);
+    toast.success(<ToastSuccess />, {
+      autoClose: 3000,
+      hideProgressBar: true,
+      closeButton: false,
+    });
+  };
 
-    const [copied, setCopied] = useState(false)
-  
-    const onCopy = () => {
-      setCopied(true)
-      toast.success(<ToastSuccess />, {
-        autoClose: 3000,
-        hideProgressBar: true,
-        closeButton: false
-      })
-    }
+  const [modal, setModal] = useState(false);
+  const [modal2, setModal2] = useState(false);
 
-  return(
-  <Col lg={12}>
-    <Card>
-      <CardBody className="invoice">
-        <div className="invoice__head">
-          <div className="invoice__head-left">
-            <div className="invoice__logo" />
-            <address className='invoice-custom-address'>
-              {invoiceAddress}
-              <CopyToClipboard className='invoice-address-custom-button' onCopy={onCopy} text={invoiceAddress}>
-            <button>
-            Copy!
-            </button>
-            </CopyToClipboard>
-            </address>
+  const toggle = () => setModal(!modal);
+  const toggle2 = () => setModal2(!modal2);
+
+  return (
+    <Col lg={12}>
+      <Card>
+        <CardBody className="invoice">
+          <div className="invoice__head">
+            <div className="invoice__head-left">
+              <div className="invoice__logo" />
+              <address className="invoice-custom-address">
+                {invoiceAddress}
+                <CopyToClipboard
+                  className="invoice-address-custom-button"
+                  onCopy={onCopy}
+                  text={invoiceAddress}
+                >
+                  <button>Copy!</button>
+                </CopyToClipboard>
+              </address>
+            </div>
+            <div className="invoice__head-right invoice__head-right-custom">
+              <h4 className="custom-h4">
+                Invoice {invoiceNumber}
+                <CopyToClipboard
+                  className="invoice-button-custom"
+                  onCopy={onCopy}
+                  text={invoiceNumber}
+                >
+                  <button>Copy!</button>
+                </CopyToClipboard>
+              </h4>
+              <p className="invoice__date">Februarie 23, 2020</p>
+              <p>Maria Morris</p>
+              <p>Project Manager at BLX</p>
+              <p>mariam@company.co</p>
+              <p>44 Shirley Ave.</p>
+              <p>West Chicago, IL 60185</p>
+            </div>
           </div>
-          <div className="invoice__head-right invoice__head-right-custom">
-            <h4 className='custom-h4'>Invoice {invoiceNumber}
-            <CopyToClipboard className='invoice-button-custom' onCopy={onCopy} text={invoiceNumber}>
-            <button>
-            Copy!
-            </button>
-            </CopyToClipboard>
-            </h4>            
-            <p className="invoice__date">Februarie 23, 2020</p>
-            <p>Maria Morris</p>
-            <p>Project Manager at BLX</p>
-            <p>mariam@company.co</p>
-            <p>44 Shirley Ave.</p>
-            <p>West Chicago, IL 60185</p>
-          </div>
-        </div>
-        <Table className="table--bordered" responsive>
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Item Name</th>
-              <th>Quantity</th>
-              <th>Unit Cost</th>
-              <th>Total</th>
-            </tr>
-          </thead>
-          <tbody>
-            {invoiceData.map((item, index) => (
-              <tr key={`index_${item.title}`}>
-                <td>{index + 1}</td>
-                <td>{item.title}</td>
-                <td>{item.quantity}</td>
-                <td>{item.cost} RON</td>
-                <td>{item.quantity * item.cost} RON</td>
+          <Table className="table--bordered" responsive>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Item Name</th>
+                <th>Quantity</th>
+                <th>Unit Cost</th>
+                <th>Total</th>
               </tr>
-            ))}
-          </tbody>
-        </Table>
-        <div className="invoice__total">
-          <p>Sub - Total amount: 126.00 RON</p>
-          <p>VAT: 20.00 RON</p>
-          <p className="invoice__grand-total">Grand Total: 146.00 RON</p>
-          <ButtonToolbar className="invoice__toolbar">
-            <Button>Print</Button>
-            <Button className="btn btn-success">Mark as Paid</Button>
-          </ButtonToolbar>
-        </div>
-      </CardBody>
-    </Card>
-  </Col>
-  )
-}
+            </thead>
+            <tbody>
+              {invoiceData.map((item, index) => (
+                <tr key={`index_${item.title}`}>
+                  <td>{index + 1}</td>
+                  <td>{item.title}</td>
+                  <td>{item.quantity}</td>
+                  <td>{item.cost} RON</td>
+                  <td>{item.quantity * item.cost} RON</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+          <div className="invoice__total">
+            <p>Sub - Total amount: 126.00 RON</p>
+            <p>VAT: 20.00 RON</p>
+            <p className="invoice__grand-total">Grand Total: 146.00 RON</p>
+            <ButtonToolbar className="invoice__toolbar">
+              <Button>Print</Button>
+              <Button onClick={toggle} className="btn btn-success">
+                Mark as Paid
+              </Button>
+              <Modal
+                size="lg"
+                style={{ maxWidth: "1000px", width: "100%" }}
+                isOpen={modal}
+                toggle={toggle}
+              >
+                <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+                <ModalBody>
+                  <Row>
+                    <Col md={12} lg={6}>
+                      <Table>
+                        <tbody>
+                          <tr>
+                            <th>Numar Factura</th>
+                            <td>{invoiceNumber}</td>
+                          </tr>
+                          <tr>
+                            <th>Data</th>
+                            <td>12.02.2021</td>
+                          </tr>
+                          <tr>
+                            <th>Nume Restaurant</th>
+                            <td>Ceva restaurant</td>
+                          </tr>
+                          <tr>
+                            <th>Nume supplier</th>
+                            <td>Kaufland</td>
+                          </tr>
+                          <tr>
+                            <th>Suma fara TVA</th>
+                            <td>130 Ron</td>
+                          </tr>
+                          <tr>
+                            <th>TVA</th>
+                            <td>20 Ron</td>
+                          </tr>
+                          <tr>
+                            <th>Suma Totala</th>
+                            <td>150 Ron</td>
+                          </tr>
+                        </tbody>
+                      </Table>
+                    </Col>
+                    <Col md={12} lg={6}>
+                      <Table>
+                        <tbody>
+                          <tr>
+                            <th>Produs :</th>
+                            <td>Oua</td>
+                          </tr>
+                          <tr>
+                            <th>Produs :</th>
+                            <td>Lapte</td>
+                          </tr>{" "}
+                          <tr>
+                            <th>Produs :</th>
+                            <td>Carne</td>
+                          </tr>{" "}
+                          <tr>
+                            <th>Produs :</th>
+                            <td>Bere</td>
+                          </tr>
+                        </tbody>
+                      </Table>
+                    </Col>
+                  </Row>
+                </ModalBody>
+                <ModalFooter>
+                  <Form style={{ width: "100%" }}>
+                    <FormGroup>
+                      <InputGroup>
+                        <InputGroupAddon addonType="prepend">
+                          <InputGroupText>Plateste partial</InputGroupText>
+                        </InputGroupAddon>
+                        <Input value="125 Ron" />
+                      </InputGroup>
+                    </FormGroup>
+                  </Form>
+                </ModalFooter>
+                <ModalFooter>
+                  <Button
+                    className="margin-left-custom"
+                    color="danger"
+                    onClick={toggle2}
+                  >
+                    Report
+                  </Button>
+                  <Modal
+                    style={{ maxWidth: "1000px", width: "100%" }}
+                    isOpen={modal2}
+                    toggle={toggle2}
+                  >
+                    <ModalHeader toggle={toggle2}>Raporteaza :</ModalHeader>
+                    <ModalBody>
+                      <FormGroup style={{ height: "400px" }}>
+                        <Input
+                          style={{ height: "100%" }}
+                          type="textarea"
+                          name="text"
+                          id="exampleText"
+                          placeholder="Subiect"
+                        />
+                      </FormGroup>
+                    </ModalBody>
+                    <ModalFooter>
+                      {" "}
+                      <Button color="success" onClick={toggle}>
+                        Trimite
+                      </Button>
+                      <Button color="secondary" onClick={toggle}>
+                        Anuleaza
+                      </Button>
+                    </ModalFooter>
+                  </Modal>
+                  <Button color="primary" onClick={toggle}>
+                    Plateste
+                  </Button>
+                  <Button color="secondary" onClick={toggle}>
+                    Anuleaza
+                  </Button>
+                </ModalFooter>
+              </Modal>
+            </ButtonToolbar>
+          </div>
+        </CardBody>
+      </Card>
+    </Col>
+  );
+};
 
 export default InvoiceTemplate;
--- src/containers/OrderHistory/components/BoxedCollapseFullWidth.jsx
+++ src/containers/OrderHistory/components/BoxedCollapseFullWidth.jsx
@@ -1,34 +1,99 @@
-import { Card, CardBody, Col } from "reactstrap";
+import { Button, Container, Row, Card, CardBody, Col } from "reactstrap";
 import Collapse from "../../../shared/components/Collapse";
 import MinimalCollapse from "./MinimalCollapse";
+import MatTableImport from "./Mattableimport";
 
 // Edesia
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { GetRestaurantOrder } from "../../../redux/actions/products";
 
-const BoxedCollapseFullWidth = ({ GetRestaurantOrder, orders }) => {
+const BoxedCollapseFullWidth = ({ GetRestaurantOrder, orders, keys, data }) => {
+  const [showOne, setShowOne] = useState(true);
+  const [showTwo, setShowTwo] = useState(false);
+
+
 
   useEffect(() => {
     GetRestaurantOrder();
   }, []);
 
   return (
-    <Col md={12} lg={12}>
-      <Card>
-        <CardBody>
-          <div className="card__title">
-            <h3 className="bold-text">📦 Receptie Marfa</h3>
-          </div>
-          {Object.keys(orders) &&
-            Object.keys(orders).map((key) => (
-              <Collapse title={key} className="with-shadow">
-                <MinimalCollapse keys={key} data={orders} />
-              </Collapse>
-            ))}
-        </CardBody>
-      </Card>
-    </Col>
+    <Container>
+      <Row>
+        <Col>
+          <Card>
+            <CardBody>
+              <Button
+       onClick={() => setShowOne(!showOne)}
+                color="secondary"
+                size="sm"
+                outline="true"
+              >
+                Receptie Marfa
+              </Button>
+              <Button
+            
+       onClick={() => setShowOne(!showOne)}
+                color="secondary"
+                size="sm"
+                outline="true"
+              >
+                Order history archive
+              </Button>
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
+      {showOne ? (
+        <Row>
+          <Col md={12} lg={6}>
+            <Card>
+              <CardBody>
+                <div className="card__title">
+                  <h3 className="bold-text">📦 Receptie Marfa</h3>
+                </div>
+                <Collapse>
+                  {Object.keys(orders) &&
+                    Object.keys(orders).map((key) => (
+                      <MinimalCollapse keys={key} data={orders} />
+                    ))}
+                </Collapse>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col md={12} lg={6}>
+            <Card>
+              <CardBody>
+                <div className="card__title">
+                  <h3 className="bold-text">📦 Receptie Marfa 2</h3>
+                </div>
+                {Object.keys(orders) &&
+                  Object.keys(orders).map((key) => (
+                    <MatTableImport keys={key} data={orders} />
+                  ))}
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      ) : (
+        <Row>
+        <Col md={12} lg={12}>
+          <Card>
+            <CardBody>
+              <div className="card__title">
+                <h3 className="bold-text">📦 Receptie Marfa 2</h3>
+              </div>
+              {Object.keys(orders) &&
+                Object.keys(orders).map((key) => (
+                  <MatTableImport keys={key} data={orders} />
+                ))}
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
+      )}
+    </Container>
   );
 };
 
